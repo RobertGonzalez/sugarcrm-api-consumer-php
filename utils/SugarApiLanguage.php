c26469a9ea417cedb63bd916ee02dee7686a25c1
Robert Gonzalez <rgonzalez@sugarcrm.com>
2014-04-13 17:22:21 -0700

    Add docBlocks to the language object

--- utils/SugarApiLanguage.php
+++ utils/SugarApiLanguage.php
@@ -1,66 +1,131 @@
 <?php
+/**
+ * Language strings interaction class. This is a read only class.
+ */
 class SugarApiLanguage
 {
+    /**
+     * The language this object is based on
+     *
+     * @var string
+     */
     protected $lang;
+
+    /**
+     * The platform for this object
+     *
+     * @var string
+     */
     protected $platform;
+
+    /**
+     * The language data that is used for getting values
+     *
+     * @var string
+     */
     protected $data = array();
-    
+
+    /**
+     * Constructor, simply sets the language and the platform
+     *
+     * @param string $lang The language for this request
+     * @param string $platform The platform for this request
+     */
     public function __construct($lang, $platform = 'base')
     {
         $this->platform = $platform;
         $this->lang = $lang;
     }
-    
+
+    /**
+     * Gets a collection from the language data based on the index of the data
+     *
+     * @param string $element The index of the language data to get
+     * @param string $label The label to get from the element
+     * @param mixed $default A default to return if the requested label wasn't found
+     * @return mixed
+     */
     protected function getLanguageElement($element, $label, $default = null)
     {
         if (!isset($this->data[$element])) {
             $this->loadLanguage();
         }
-        
+
         if (isset($this->data[$element][$label])) {
             return $this->data[$element][$label];
         }
-        
+
         return $default;
     }
-    
+
+    /**
+     * Gets a string from the app strings collection
+     *
+     * @param string $label The label to get
+     * @param mixed $default A default to return if the requested label wasn't found
+     * @return mixed
+     */
     public function getAppString($label, $default = null)
     {
         return $this->getLanguageElement('app_strings', $label, $default);
     }
-    
+
+    /**
+     * Gets a string from the app list strings collection
+     *
+     * @param string $label The label to get
+     * @param mixed $default A default to return if the requested label wasn't found
+     * @return mixed
+     */
     public function getAppListString($label, $default = null)
     {
         return $this->getLanguageElement('app_list_strings', $label, $default);
     }
-    
+
+    /**
+     * Gets a string from the module strings collection
+     *
+     * @param string $module The module to get the string for
+     * @param string $label The label to get
+     * @param mixed $default A default to return if the requested label wasn't found
+     * @return mixed
+     */
     public function getModuleString($module, $label, $default = null)
     {
         if (!isset($this->data['mod_strings'])) {
             $this->loadLanguage();
         }
-        
+
         if (isset($this->data['mod_strings'][$module][$label])) {
             return $this->data['mod_strings'][$module][$label];
         }
-        
+
+        // If there was no module string found, try the app strings
         $appString = $this->getAppString($label, $default);
         if ($appString == $label) {
             return $default;
         }
-        
+
         return $appString;
     }
-    
+
+    /**
+     * Loads the language data either from cache or from the api
+     */
     public function loadLanguage()
     {
         $this->data = $this->getLanguage();
     }
-    
+
+    /**
+     * Gets the language data either from cache or from the api
+     *
+     * @return array
+     */
     public function getLanguage()
     {
         $data = $this->getLanguageFromCache();
-        
+
         if (empty($data)) {
             $api = SugarApiUtil::getInstance();
             $reply = $api->call("lang/{$this->lang}?platform={$this->platform}");
@@ -69,7 +134,7 @@ class SugarApiLanguage
                 $this->writeLanguageToCache($data);
             }
         }
-        
+
         return $data;
     }
 
